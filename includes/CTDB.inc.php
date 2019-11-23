e78c1287b0a5ff8ef5f7a9e2ea8ba9de3c45204f
Miguel Tapia <mtapiafdez@gmail.com>
2019-11-22 22:31:56 -0500

    A little more cleanup

--- includes/CTDB.inc.php
+++ includes/CTDB.inc.php
@@ -1,8 +1,8 @@
 <?php
     $servername = "localhost";
-    $dBUsername = "mtapia";
-    $dBPassword = "MiguelTapia";
-    $dBName = "eninvem";
+    $dBUsername = "";
+    $dBPassword = "";
+    $dBName = "";
 
     $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
